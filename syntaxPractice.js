9198a273cd30547ae6d4f952674acfe65e52feaf
BrennanStopnik <brennan.stopnik@codeimmersives.com>
2022-06-23 16:21:02 -0400

    create syntax practice

---
+++ syntaxPractice.js
@@ -0,0 +1,172 @@
+// Syntax Practice
+
+
+/* Example with answer */
+/*
+Create an array called numbers with the numbers 1,2, and 3
+
+Answer Format: let _____ = [__, __, __];
+*/
+
+let numbers = [1, 2, 3]; //Answer
+
+/*
+Create an array called cardGames that includes the following items as strings in order: Go Fish, Poker, Palace
+
+let ______ = ['_____', '_____', '______'];
+*/
+
+/*
+Now get the value at index 0
+console.log(_____[_]);
+*/
+
+/*
+Now get the value at index 1
+console.log(_____[_]);
+*/
+
+/*
+Now get the item 'Palace' from the array
+console.log(_____[_])
+*/
+
+/*
+Create an array called settings and include the following items in order: Plains, Swamp, Ocean, Mountains, Lake
+
+let ______ = ['_____', '_____', '______','______','______'];
+*/
+
+/*
+Now get the value at index 0
+console.log(_____[_]);
+*/
+
+/*
+Now get the value at index 1
+console.log(_____[_]);
+*/
+
+/*
+Now add an item to the end of the array called 'Desert'
+_____.___('____');
+*/
+
+
+/*
+Now get the last item in the array using the array's length
+console.log(_____[____.length - _]);
+*/
+
+/*
+Now print out the entire array
+console.log(_____)
+*/
+
+
+/*
+Create an array called mediaTypes and include the following items in order: TV, Movies, Music, Video Games
+
+Now get the value at index 0
+console.log(_____[_]);
+*/
+
+/*
+Now get the last item in the array using the array's length
+console.log(_____[____.length - _]);
+*/
+
+/*
+Now remove the last item from the array
+_____.____()
+*/
+
+/*
+Now print out the entire array
+console.log(_____)
+*/
+
+
+
+/*
+Create an array called officeItems and include the following items in order: Desk, Laptop, Pen, Notebook, Binder
+
+Now get the value at index 0
+console.log(_____[_]);
+*/
+
+/*
+Now change the value at index 1 to Tablet
+______[_] = _________
+*/
+
+/*
+Now change the item Pen to Pencil
+______[_] = _________
+*/
+
+/*
+Now print out the entire array
+console.log(_____)
+*/
+
+
+/*
+For this problem we will use the following array
+let activities = ['Skydiving', 'Rock Climbing', 'Snowboarding', 'Escape Room']
+
+Print out every item in the array 1 at a time using a regular for loop
+
+for(__i = _; i _ ______; i__){
+	console.log(_____[_]);
+}
+*/
+
+/*
+Now print out the same array in the following format:
+1. Skydiving
+2. Rock Climbing
+3. Snowboarding
+4. Escape Room
+
+//concatenation
+for(__i = _; i _ ______; i__){
+	console.log(____ + _____[_]);
+}
+*/
+
+/*
+//string interpolation
+for(__i = _; i _ ______; i__){
+	console.log(`${___}_ ${ _____[_]}`);
+}
+*/
+
+/*
+For the following problems, let's use the following array
+let nums = [7, 22, 30, 5, 15, 39, 26]
+
+Print out every number in the array one at a time using a regular for loop
+for(__i = _; i _ ______; i__){
+	console.log(_____[_]);
+}
+*/
+
+/*
+Now print out only the numbers greater than 20 inside a loop
+for(__i = _; i _ ______; i__){
+	if(____[_] > _____){
+	    console.log(_____[_]);
+	}
+}
+*/
+
+/*
+Now print out only the numbers at odd indexes
+
+for(__i = _; i _ ______; i__){
+	if(___ % 2  !== _){
+	    console.log(_____[_]);
+	}
+}
+*/
